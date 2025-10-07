ff657225914d48a92a2f1c42202da6457c65d07d
byanca lourenço <159471845+byancalourenco@users.noreply.github.com>
2025-10-07 09:06:30 -0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <form action="processa_login.php" method="POST">
+       Email: <input type="text" name="email" id=""><br>
+       Senha: <input type="text" name="senha" id=""><br>
+        <input type="submit" value="Logar">
+    </form>
+</body>
+</html>
---
+++ processa_login.php
@@ -0,0 +1,41 @@
+<?php
+
+   session_start();
+   include_once 'conexao.php';
+
+   $email = $_POST['email'];
+   $senha = $_POST['senha'];
+
+   $consulta = "SELECT * FROM usuarios WHERE email =  :email AND senha = :senha";
+
+    $stmt = $pdo->prepare($consulta);
+
+    // vincula os parâmetros
+    $stmt->bindParam(':email', $email);
+    $stmt->bindParam(':senha', $senha);
+
+    // executa a consulta
+    $stmt->execute();
+
+    // obtém o num de registro encontrados 
+    $num_registro = $stmt->rowCount();
+
+    // obtém o resultado
+    $resultado = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    // exibe o resultado 
+    var_dump($resultado);
+
+    if($num_registro == 0){
+        echo "Vc not está cadastrado, sorry";
+        header('Location: index.php');
+    }else{
+        $_SESSION['id'] = $resultado['id'];
+        $_SESSION['nome'] = $resultado['nome'];
+        $_SESSION['email'] = $resultado['email'];
+        header('Location: restrita.php');
+
+        echo "Acesso permitido para a restrita.php";
+    }
+    
+?>
